f4c44eb824ce154a830d7927a8505b5a93a05699
sjeyte <148445316+sjeyte@users.noreply.github.com>
2023-12-11 21:38:27 -0800

    Update CSV_Merge.R

--- CSV_Merge.R
+++ CSV_Merge.R
@@ -7,12 +7,32 @@ income_df <- read.csv("PER_CAPITA_INCOME_and_AGGREGATE_INCOME_IN_THE_PAST_12_MON
 
 df <- left_join(income_df, tree_df, join_by(GEOID == GEOID10), na_matches = "never")
 
-colnames(df)[4] <- "Total Population"
-colnames(df)[5] <- "Aggregate Income"
-colnames(df)[6] <- "Per Capita Income"
+colnames(df)[4] <- "Total_Population"
+colnames(df)[5] <- "Aggregate_Income"
+colnames(df)[6] <- "Per_Capita_Income"
+
+# Data Cleaning
+df <- filter(df, !is.na(df$TreeCanopy_2016_Percent))
+df <- filter(df, !is.na(df$TreeCanopy_2021_Percent))
+df <- filter(df, !is.na(df$Per_Capita_Income))
+df <- filter(df, !is.na(df$GEN_ALIAS))
 
 df$maj_TC <- ifelse(df$TC_E_P > 50, TRUE, FALSE)
 df$Imperv_TC_Diff <- df$TC_E_P - df$Imperv_P
+
+# Explicitly create the TreeCanopy_Change column
+df <- mutate(df, TreeCanopy_Change = df$TreeCanopy_2021_Percent - df$TreeCanopy_2016_Percent)
+
+# Grouping and Summarizing
+grouped_df <- group_by(df, TRACT_LABEL, ACS_VINTAGE)
+summarized_df <- summarise(
+  grouped_df,
+  Total_Population = sum(`Total_Population`, na.rm = TRUE),
+  Average_Income = mean(`Per_Capita_Income`, na.rm = TRUE),
+  Average_Tree_Canopy_Change = mean(TreeCanopy_Change, na.rm = TRUE),
+  Major_Tree_Canopy = sum(maj_TC, na.rm = TRUE)
+)
+
 
 write.csv(df, "JoinedDF.csv", row.names=FALSE)
 
